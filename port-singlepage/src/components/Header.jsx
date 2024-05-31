b6b2829a465546e2556d9f22b9f94dc5d48e9f97
Ray Thomas <rthomas5678@gmail.com>
2024-05-30 21:21:43 -0700

    added full color pallete and swapped fonts

--- port-singlepage/src/components/Header.jsx
+++ port-singlepage/src/components/Header.jsx
@@ -1,10 +1,13 @@
 function Header() {
     return(
+        <>
         <div className="header">
             <p> Hello! My name is</p>
             <h1>Ray Thomas</h1>
-            <p> I'm a web developer from Phoenix, AZ</p>
+            <p> I'm a <span>Web Developer</span> & <span>Graphic Designer</span> from Phoenix, AZ</p>
         </div>
+        </>
+        
     )
 }
 
