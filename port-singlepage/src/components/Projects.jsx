469e5458e501c319725b1c80e1369942d1e249ab
Ray Thomas <rthomas5678@gmail.com>
2024-07-02 21:20:31 -0700

    added content and design to the projects section

--- port-singlepage/src/components/Projects.jsx
+++ port-singlepage/src/components/Projects.jsx
@@ -4,24 +4,38 @@ function Projects () {
             <h1>My Projects</h1>
             <section className="project-row">
                 <section className="project-ind">
-                    <h3> Project One </h3>
+                    <div className="project-title">
+                        <h3> Streaming App </h3>
+                    </div>
+                    <div className="project-thumbnail">
+                        <img className="project-image" src="/streaming-app.png"></img>
+                    </div>
+                    <p>
+                        This front-end project allows users to discover trending movies and tv shows, as well as
+                        search for their favorites. Utilizing the Movie Database API, this projects allows users to
+                        find almost any type of film mdeia they can think of.
+                    </p>
+                    <div className="btns">
+                        <a className="project-link" href="https://github.com/raythomass/streaming-app">GitHub</a>
+                        <a className="project-link" href="#">Deployed App</a>
+                    </div>
                 </section>
                 <section className="project-ind">
-                    <h3> Project Two </h3>
+                    <h3> Ray's Pizzeria </h3>
                 </section>
                 <section className="project-ind">
-                    <h3> Project Three </h3>
+                    <h3> Football Database </h3>
                 </section>
             </section>
             <section className="project-row">
                 <section className="project-ind">
-                    <h3> Project Four </h3>
+                    <h3> Text Editor </h3>
                 </section>
                 <section className="project-ind">
-                    <h3> Project Five </h3>
+                    <h3> Weather Forecast </h3>
                 </section>
                 <section className="project-ind">
-                    <h3> Project Six </h3>
+                    <h3> Daily Scheduler </h3>
                 </section>
             </section> 
         </section>
