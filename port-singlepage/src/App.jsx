7a62bbd03875528e24d411c7acd26a95b92e9a2d
Ray Thomas <rthomas5678@gmail.com>
2024-12-31 18:16:44 -0700

    added libraries for css and components

--- port-singlepage/src/App.jsx
+++ port-singlepage/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
